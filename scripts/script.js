d389abfeb695508d99468d46b70ee3ff6588063e
Andrianov Roman <r_andrianov02@mail.ru>
2023-11-23 15:58:52 +0300

    added new project

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+
+let mainBtn = document.querySelector('#mainBtn');
+let collection = document.querySelector('#collection');
+
+mainBtn.addEventListener('click', () => {
+    collection.scrollIntoView();
+});
+
